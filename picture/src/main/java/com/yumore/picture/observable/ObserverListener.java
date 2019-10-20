0323584c7be7213b36ac8ca11d756993606d080a
Nathaniel <nathanwriting@126.com>
2019-10-20 20:27:30 +0800

    merge modules

---
+++ picture/src/main/java/com/yumore/picture/observable/ObserverListener.java
@@ -0,0 +1,19 @@
+package com.yumore.picture.observable;
+
+import com.yumore.picture.entity.LocalMedia;
+import com.yumore.picture.entity.LocalMediaFolder;
+
+import java.util.List;
+
+/**
+ * author：luck
+ * project：PictureSelector
+ * package：com.luck.picture.lib.observable
+ * email：893855882@qq.com
+ * data：17/1/16
+ */
+public interface ObserverListener {
+    void observerUpFoldersData(List<LocalMediaFolder> folders);
+
+    void observerUpSelectsData(List<LocalMedia> selectMedias);
+}
